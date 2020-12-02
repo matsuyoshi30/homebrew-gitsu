69f07106b3c83fb2b139a2dc047d4de7a64ff5b7
matsuyoshi30 <sfbgwm30@gmail.com>
2020-12-02 23:19:17 +0900

    Fix formula

--- gitsu.rb
+++ gitsu.rb
@@ -2,7 +2,7 @@ class Gitsu < Formula
   desc "Switch git user easily"
   homepage "https://github.com/matsuyoshi30/gitsu"
   url "https://github.com/matsuyoshi30/gitsu/releases/download/v0.1.0/gitsu_0.1.0_darwin_x86_64.tar.gz"
-  sha256 "d28de71deb5cc2f87cb44aaa317a5b3cf825385c22860965733099adb5521249"
+  sha256 "2acfe7b77b27090b676e90bccba883cdff4bdaac7e845647d5eb50b1de7f296a"
 
   def install
     bin.install "git-su"
